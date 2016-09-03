4712a6cb141d255ae1f215c6183ee3d3e9237b64
Dan Watkins <dwatkins@valpineware.com>
2016-09-02 22:15:26 -0500

    Added project for Chapter3

---
+++ Atgp/Atgp.Chapter3/MainForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace Atgp.Chapter3
+{
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            ClientSize = new Size(1280, 720);
+        }
+    }
+}
---
+++ Atgp/Atgp.Chapter3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Eto;
+using Eto.Forms;
+
+namespace Atgp.Chapter3
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Platform.Detect).Run(new MainForm());
+        }
+    }
+}
