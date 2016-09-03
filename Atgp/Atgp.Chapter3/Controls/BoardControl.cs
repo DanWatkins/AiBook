c55d0bbaa36440acccd36d87606693e53965175e
Dan Watkins <dwatkins@valpineware.com>
2016-09-02 23:19:10 -0500

    Copied and adapted BoardControl from BlockWall project.
    https://github.com/DanWatkins/AiBook

---
+++ Atgp/Atgp.Chapter3/Controls/BoardControl.cs
@@ -0,0 +1,89 @@
+﻿using Eto.Drawing;
+using Eto.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atgp.Chapter3.Controls
+{
+    public class BoardControl : Drawable
+    {
+        public Color BaseColor { get; set; } = Colors.Black;
+
+        public Color TileColor { get; set; } = Colors.Black;
+
+        public Color GrooveColor { get; set; } = Colors.Gray;
+
+        public Size TileSize { get; }
+
+        public Size BoardSize { get; }
+
+        public int GrooveThickness { get; }
+
+        public int BorderThickness { get; }
+
+        /// <param name="tileSize">Size of each tile in pixel units.</param>
+        /// <param name="boardSize">Size of the board in tile units.</param>
+        public BoardControl(Size tileSize, Size boardSize, int grooveThicknes=1, int borderThickness=1)
+        {
+            InitializeBoard();
+
+            TileSize = tileSize;
+            BoardSize = boardSize;
+            GrooveThickness = grooveThicknes;
+            BorderThickness = borderThickness;
+        }
+
+        private void InitializeBoard()
+        {
+            int boardWidthPx = BoardSize.Width * (TileSize.Width + GrooveThickness) - GrooveThickness;
+            int boardHeightPx = BoardSize.Height * (TileSize.Height + GrooveThickness) - GrooveThickness;
+
+            int baseWidthPx = boardWidthPx + BorderThickness * 2;
+            int baseHeightPx = boardHeightPx + BorderThickness * 2;
+
+            //update control size
+            Width = baseWidthPx;
+            Height = baseHeightPx;
+
+            Paint += (sender, args) =>
+            {
+                var rect = new RectangleF(ClientSize);
+
+                //draw the base
+                args.Graphics.SaveTransform();
+                args.Graphics.SetClip(new RectangleF(
+                        0, 0,
+                        baseWidthPx,
+                        baseHeightPx));
+                args.Graphics.FillRectangle(BaseColor, rect);
+
+                //draw the grooves
+                args.Graphics.SaveTransform();
+                args.Graphics.SetClip(new RectangleF(
+                        BorderThickness, BorderThickness,
+                        boardWidthPx,
+                        boardHeightPx));
+                args.Graphics.FillRectangle(GrooveColor, rect);
+
+                //draw the tiles
+                for (int w = 0; w < BoardSize.Width; w++)
+                {
+                    for (int h = 0; h < BoardSize.Height; h++)
+                    {
+                        args.Graphics.SetClip(new RectangleF(
+                                w * (TileSize.Width + GrooveThickness) + BorderThickness,
+                                h * (TileSize.Height + GrooveThickness) + BorderThickness,
+                                TileSize.Width,
+                                TileSize.Height));
+                        args.Graphics.FillRectangle(TileColor, rect);
+                    }
+                }
+
+                args.Graphics.RestoreTransform();
+            };
+        }
+    }
+}
--- Atgp/Atgp.Chapter3/MainForm.cs
+++ Atgp/Atgp.Chapter3/MainForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Eto.Forms;
 using Eto.Drawing;
+using Atgp.Chapter3.Controls;
 
 namespace Atgp.Chapter3
 {
@@ -8,7 +9,22 @@ namespace Atgp.Chapter3
     {
         public MainForm()
         {
+            Title = "Genetic Pathfinder";
             ClientSize = new Size(1280, 720);
+
+            var stackLayout = new StackLayout
+            {
+                HorizontalContentAlignment = HorizontalAlignment.Stretch
+            };
+
+            stackLayout.Items.Add(
+                new StackLayoutItem(
+                    new BoardControl(
+                        tileSize: new Size(64, 64),
+                        boardSize: new Size(16, 8)), 
+                    true));
+
+            Content = stackLayout;
         }
     }
 }
